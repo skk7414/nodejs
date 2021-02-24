3ac4a9c47873e81d7b7797b0ff44c4807cfb8875
skk7414 <skk7414@naver.com>
2021-02-24 22:42:49 +0900

    add bot state

--- index.js
+++ index.js
@@ -263,5 +263,30 @@ module.exports = {
   },
 };
 
-
+if (message.content === '!코로나') {
+  let url = "https://apiv2.corona-live.com/stats.json"
+  request(url, (error, response, body) => {
+      let overview = JSON.parse(response.body).overview;
+      overview = {
+          total_confirmed_person: overview.confirmed[0], // 총 확진자수
+          yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
+  
+          current_confirmed_person: overview.current[0], // 현재 확진자수
+          current_confirmed_person_diff: overview.current[1], // diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+      }
+  
+      let embed = new Discord.MessageEmbed()
+      embed.setTitle('코로나 라이브 홈페이지')
+      embed.setURL('https://corona-live.com')
+      embed.setColor('#FF8000')
+      embed.setDescription('코로나 정보입니다')
+      embed.addField(`대한민국 총 확진자 수`, `${overview.total_confirmed_person}명`, true)
+      embed.addField(`어제 확진자 수`, overview.yesterday_confirmed_person + `명`, true)
+      embed.addField(`오늘 확진자 수`, overview.current_confirmed_person + `명`, true)
+      // embed.addField(`오늘 어제지금시간   -   현재지금시간의 확진자`, overview.current_confirmed_person_diff + `명`, true)
+      message.channel.send(embed)
+  
+    })
+  }
+  
 client.login(token);
