e8e5b3abf217dd22c752b5f6a6d5538afe094b3d
skk7414 <skk7414@naver.com>
2021-02-25 01:21:35 +0900

    bot upload

--- index.js
+++ index.js
@@ -288,47 +288,5 @@ if (message.content === '!코로나') {
   
     })
   }
-  
-module.exports = {
-  name: "복권",
-  description: "play a game of rock, paper and scissors",
-  run: async(client, message, args) => {
-      let embed = new discord.MessageEmbed()
-      .setTitle("복권 게임")
-      .setDescription("복권 놀이! 이것은 복권을  놀이 입니다. (가위바위보기능을 이용한겁니다. 가위바위보기능이랑 동일합니다.)")
-      .setTimestamp()
-      let msg = await message.channel.send(embed)
-      await msg.react(":dollar:")
-      await msg.react(":oncoming_police_car:")
-      await msg.react(":credit_card:")
 
-      const filter = (reaction, user) => {
-          return [':dollar:', ':oncoming_police_car:', ':credit_card:'].includes(reaction.emoji.name) && user.id === message.author.id;
-      }
-
-      const choices = [':dollar:', ':oncoming_police_car:', ':credit_card:']
-      const me = choices[Math.floor(Math.random() * choices.length)]
-      msg.awaitReactions(filter, {max:1, time: 60000, error: ["time"]}).then(
-          async(collected) => {
-              const reaction = collected.first()
-              let result = new discord.MessageEmbed()
-              .setTitle("결과")
-              .addField("당신의 선택", `${reaction.emoji.name}`)
-              .addField("내 선택", `${me}`)
-          await msg.edit(result)
-              if ((me === ":dollar:" && reaction.emoji.name === ":oncoming_police_car:") ||
-              (me === ":credit_card:" && reaction.emoji.name === ":dollar:") ||
-              (me === ":oncoming_police_car:" && reaction.emoji.name === ":credit_card:")) {
-                  message.reply("복권 날라가셨네요 ㅋㅋ");
-          } else if (me === reaction.emoji.name) {
-              return message.reply("네 복권 날리셨네요 ㅋㅋ");
-          } else {
-              return message.reply("님이 이겨서 복권을 가지셨어요");
-          }
-      })
-      .catch(collected => {
-              message.reply('제 시간에 응답하지 않았으므로 처리가 취소되었습니다!');
-          })
-}
-}
 client.login(token);
